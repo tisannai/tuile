9d7670d4e275ec941e82b3bb92dd7782d0d8a9d9
Tero Isannainen <tero.isannainen@gmail.com>
2020-12-08 23:28:17 +0200

    New acronyms.

--- tuile/acronym.scm
+++ tuile/acronym.scm
@@ -1,6 +1,8 @@
 (define-module (tuile acronym)
   #:use-module ((tuile utils) #:select (assoc-has-key? assoc-update! assoc-repeat!))
-  #:use-module ((srfi srfi-1) #:select (drop-right last))
+  #:use-module ((srfi srfi-1) #:select (drop-right last concatenate))
+  #:use-module (srfi srfi-43)
+  #:use-module ((ice-9 hash-table) #:select (alist->hash-table))
   #:export
   (
    h-new
@@ -13,32 +15,62 @@
    h-values
    h-entries
    h-count
+
    a=
    a.
    a-key?
    a-update!
    a-repeat!
+   a->hash
+   a-len
 
    s-new
+   s->l
+   s->n
    s=
    s.
+   s+
+   s&
+   s?
    s=?
-   s->l
-   s-append
-   s-concat
+   s-len
    s-join
 
    l-new
    l->s
+   l->v
    l=
    l.
+   l+
+   l&
+   l?
+   l-len
+   l-any?
+   l-empty?
    l-rm
    l-rm!
-   l-append
    l-tail
    l-head
    l-drop
    l-last
+   l-copy
+
+   n->s
+   n?
+   nie
+   nei
+
+   v-new
+   v->l
+   v=
+   v.
+   v+
+   v&
+   v?
+   v-len
+   v-any?
+   v-empty?
+   v->l
    ))
 
 
@@ -59,26 +91,57 @@
 (define a-key? assoc-has-key?)
 (define a-update! assoc-update!)
 (define a-repeat! assoc-repeat!)
+(define a->hash alist->hash-table)
+(define a-len length)
 
 
 (define s-new make-string)
+(define s->l string->list)
+(define s->n string->number)
 (define s= string-set!)
 (define s. substring)
+(define s+ string-append)
+(define s& string-concatenate)
+(define s? string?)
 (define s=? string=?)
-(define s->l string->list)
-(define s-append string-append)
-(define s-concat string-concatenate)
+(define s-len string-length)
 (define s-join string-join)
 
 
 (define l-new list)
 (define l->s list->string)
+(define l->v list->vector)
 (define l= list-set!)
 (define l. list-ref)
+(define l+ append)
+(define l& concatenate)
+(define l? list?)
+(define l-len length)
+(define l-any? pair?)
+(define l-empty? null?)
 (define l-rm delete)
 (define l-rm! delete!)
-(define l-append append)
 (define l-tail list-tail)
 (define l-head list-head)
 (define l-drop drop-right)
 (define l-last last)
+(define l-copy list-copy)
+
+
+(define n->s number->string)
+(define n? number?)
+(define nie inexact->exact)
+(define nei exact->inexact)
+
+
+(define v-new vector)
+(define v->l vector->list)
+(define v= vector-set!)
+(define v. vector-ref)
+(define v+ vector-append)
+(define v& vector-concatenate)
+(define v? vector?)
+(define v-len vector-length)
+(define v-any? (lambda (v) (not (vector-empty? v))))
+(define v-empty? vector-empty?)
+(define v->l vector->list)
