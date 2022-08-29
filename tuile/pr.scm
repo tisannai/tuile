33fa55d9cd616902da1ac0274d95724bc83e19c9
Tero Isannainen <tero.isannainen@gmail.com>
2022-08-29 17:16:56 +0300

    Pretty printing for records.

--- tuile/pr.scm
+++ tuile/pr.scm
@@ -1,5 +1,7 @@
 (define-module (tuile pr)
   #:use-module ((ice-9 pretty-print) #:select (pretty-print))
+  #:use-module (rnrs records inspection)
+  #:use-module (rnrs records procedural)
   #:export
   (pr
    prp
@@ -197,10 +199,48 @@
   (for-each pd datums)
   (exit 0))
 
-(define pp pretty-print)
+(define (pretty-print-including-records rec . initial-indent)
+  (define indent-step 2)
+  (define (space indent) (make-string indent #\ ))
+  (define (print-with-indent rec indent)
+    (define-syntax-rule (next-indent) (+ indent indent-step))
+    (cond
+     ((list? rec)
+      (pr (space indent) "(")
+      (for-each (lambda (datum) (print-with-indent datum (next-indent))) rec)
+      (pr (space (next-indent)) ")"))
+     ((vector? rec)
+      (pr (space indent) "[")
+      (for-each (lambda (datum) (print-with-indent datum (next-indent))) (vector->list rec))
+      (pr (space (next-indent)) "]"))
+     ((record? rec)
+      (let ((rtd (record-rtd rec)))
+        ;; (pr "IS REC")
+        (pr (space indent) (record-type-name rtd) " {")
+        (for-each (lambda (name-value)
+                    (pr (space (next-indent)) (car name-value) ":")
+                    (print-with-indent (cdr name-value) (+ indent (* indent-step 2))))
+                  (let loop ((fields (vector->list (record-type-field-names rtd)))
+                             (index 0)
+                             (ret '()))
+                    (if (pair? fields)
+                        (loop (cdr fields)
+                              (1+ index)
+                              (cons (cons (car fields)
+                                          ((record-accessor rtd index) rec))
+                                    ret))
+                        (reverse ret))))
+        (pr (space (next-indent)) "}")))
+     (else
+      ;; (pr "NOT REC")
+      (pretty-print rec #:per-line-prefix (space indent)))))
+  (print-with-indent rec (if (pair? initial-indent) (car initial-indent) 0)))
+
+(define (pp . datums)
+  (for-each pretty-print-including-records datums))
 
 (define (ppe . datums)
-  (for-each (lambda (datum) (pp datum)) datums)
+  (apply pp datums)
   (exit 0))
 
 ;; Line print from args.
