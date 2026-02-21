a4fcd4258db2478b5b12c25d2d05c00f9c199c62
Tero Isannainen <tero.isannainen@gmail.com>
2026-02-21 14:01:37 +0200

    Added c-utils module.

---
+++ tuile/c-utils.scm
@@ -0,0 +1,36 @@
+(define-module (tuile c-utils)
+  #:use-module ((tuile edit) #:prefix #{eu:}#)
+  #:export
+  (
+   get-function-prototypes
+   base64-encode
+   base64-decode
+   ))
+
+
+(define (get-function-prototypes filename static)
+  ;;                  int           *         ilog2              (  int  )
+  ;;                  \\             \     \   \                  \  \    \   \
+  (let* ((re-fn-start "^([a-zA-Z0-9_]+[*]*)([ ]+[a-zA-Z0-9_]+[*]*)+\\(.*(,|\\))$")
+         (re-fn-end   "\\)$")
+         (fn-start? (lambda (eu)
+                      ((if static identity not) (eu:match-re? eu "^static"))))
+         (eu (eu:read filename)))
+    (let loop-lines ((protos '()))
+      (if (eu:search-re eu re-fn-start)
+          (if (fn-start? eu)
+              (loop-lines (append protos
+                                  (let loop-proto ((lines '()))
+                                    (let ((cur-line (eu:get eu)))
+                                      (if (eu:match-re? eu re-fn-end)
+                                          (begin
+                                            (eu:step eu)
+                                            (append lines (list (string-append cur-line ";"))))
+                                          (begin
+                                            (eu:step eu)
+                                            (loop-proto (append lines
+                                                                (list cur-line)))))))))
+              (begin
+                (eu:step eu)
+                (loop-lines protos)))
+          protos))))
